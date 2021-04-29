7e3e70266eb5a8c8d9cab559928f3c13e06f7d1d
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-04-29 10:14:55 +0200

    Init commit

---
+++ src/test/java/hu/nive/ujratervezes/kepesitovizsga/jurassic/JurassicParkTest.java
@@ -0,0 +1,30 @@
+package hu.nive.ujratervezes.kepesitovizsga.jurassic;
+
+public class JurassicParkTest {
+
+//    private JurassicPark jurassicPark;
+//
+//    @BeforeEach
+//    void init() throws SQLException {
+//        MariaDbDataSource dataSource;
+//        dataSource = new MariaDbDataSource();
+//        dataSource.setUrl("jdbc:mariadb://localhost:3306/employees?useUnicode=true");
+//        dataSource.setUser("employees");
+//        dataSource.setPassword("employees");
+//
+//        Flyway flyway = Flyway.configure().dataSource(dataSource).load();
+//
+//        flyway.clean();
+//        flyway.migrate();
+//
+//        jurassicPark = new JurassicPark(dataSource);
+//
+//    }
+//
+//
+//    @Test
+//    void test_check() {
+//        assertEquals(List.of("Hypsilophodontida", "Maiasaurus", "Velociraptor"), jurassicPark.checkOverpopulation());
+//    }
+
+}
