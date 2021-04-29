2f9edad252327cf560f350227e8afb092035f1c4
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-04-29 11:42:53 +0200

    Add 'Jurassic' solution

---
+++ src/main/java/jurassic/JurassicPark.java
@@ -0,0 +1,37 @@
+package jurassic;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JurassicPark {
+
+    DataSource dataSource;
+
+    public JurassicPark(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<String> checkOverpopulation() {
+        List<String> result = new ArrayList<>();
+        String query = "SELECT `breed` FROM `dinosaur` WHERE `expected` != `actual` ORDER BY `breed`";
+
+        try (
+                Connection conn = dataSource.getConnection();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(query)
+        ) {
+            while (rs.next()) {
+                result.add(rs.getString("breed"));
+            }
+        } catch (SQLException sqle) {
+            throw new IllegalStateException("Cannot query!", sqle);
+        }
+
+        return result;
+    }
+}
